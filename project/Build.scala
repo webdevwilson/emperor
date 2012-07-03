e84a0293e726a433e13113da1ec50828df957799
Cory G Watson <gphat@onemogin.com>
2012-07-03 18:07:05 -0500

    Upgradin'

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
+    "org.scala-lang" % "scala-compiler"         % "2.9.1",
     "org.specs2"    %% "specs2"                 % "1.8.2"           % "test",
     "mysql"         % "mysql-connector-java"    % "5.1.19",
     "org.mindrot"   % "jbcrypt"                 % "0.3m",
@@ -15,6 +16,7 @@ object ApplicationBuild extends Build {
   )
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    scalaVersion := "2.9.1"
   )
 }
