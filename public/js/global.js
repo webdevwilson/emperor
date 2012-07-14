e776e3e83f0754ff4433fcc12763ad3285e1963c
Cory G Watson <gphat@onemogin.com>
2012-07-14 09:18:38 -0700

    More js.

--- public/js/global.js
+++ public/js/global.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
       })
       var ival = item.children("input:hidden").val()
       var iname = item.attr("data-name")
-      $("#workflow-inuse").append("<li data-name=\"" + iname + "\"><input type=\"hidden\" name=\"statusId\" value=\"" + ival + "\"><div class=\"btn-group\" style=\"display: inline; padding: 0; margin: 0\"><button class=\"btn btn-mini fire\"><i class=\"icon-arrow-up\"></i></button><button class=\"btn btn-mini fire\"><i class=\"icon-arrow-down\"></i></button></div>" + iname + "<button class=\"btn btn-mini btn-danger pull-right fire remover\"><i class=\"icon-remove icon-white\"></i></button></li>")
+      $("#workflow-inuse").append("<li data-name=\"" + iname + "\"><input type=\"hidden\" name=\"statusId\" value=\"" + ival + "\"><div class=\"btn-group\" style=\"display: inline; padding: 0; margin: 0\"><button class=\"btn btn-mini fire upper\"><i class=\"icon-arrow-up\"></i></button><button class=\"btn btn-mini fire downer\"><i class=\"icon-arrow-down\"></i></button></div> " + iname + "<button class=\"btn btn-mini btn-danger pull-right fire remover\"><i class=\"icon-remove icon-white\"></i></button></li>")
     }
     event.preventDefault();
   })
