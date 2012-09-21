9f57a541371d6feeb76375ee5d14f24156612043
Cory G Watson <gphat@onemogin.com>
2012-09-20 23:01:14 -0500

    Bump version number. Refs EMP-66

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import sbtbuildinfo.Plugin._
 object ApplicationBuild extends Build {
 
   val appName         = "emperor"
-  val appVersion      = "0.0.10"
+  val appVersion      = "0.0.11"
 
   val appDependencies = Seq(
     "org.scala-lang" % "scala-compiler"         % "2.9.1",
