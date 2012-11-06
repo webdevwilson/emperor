ca4097faeb7c1d8b6e1de2ba7540126b75b532df
Cory G Watson <gphat@onemogin.com>
2012-11-06 17:08:28 -0600

    Read as UTC from the DB and add the timeago.

--- app/emp/util/AnormExtension.scala
+++ app/emp/util/AnormExtension.scala
@@ -11,8 +11,8 @@ object AnormExtension {
   implicit def rowToDateTime: Column[DateTime] = Column.nonNull { (value, meta) =>
     val MetaDataItem(qualified, nullable, clazz) = meta
     value match {
-      case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime))
-      case d: java.sql.Date => Right(new DateTime(d.getTime))
+      case ts: java.sql.Timestamp => Right(new DateTime(ts.getTime).withZoneRetainFields(DateTimeZone.UTC))
+      case d: java.sql.Date => Right(new DateTime(d.getTime).withZoneRetainFields(DateTimeZone.UTC))
       case str: java.lang.String => Right(dateFormatGeneration.parseDateTime(str))
       case _ => Left(TypeDoesNotMatch("Cannot convert " + value + ":" + value.asInstanceOf[AnyRef].getClass) )
     }
