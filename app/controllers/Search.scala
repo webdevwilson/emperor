722cd360e76907443fe934540d6120ff360b0dc8
Cory G Watson <gphat@onemogin.com>
2013-01-20 15:14:02 -0600

    Redirect to exact ticket if we get a search for one. Refs EMP-219

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -2,7 +2,7 @@ package controllers
 
 import emp._
 import collection.JavaConversions._
-import models.SearchModel
+import models._
 import org.elasticsearch.search.facet.terms.strings._
 import play.api.mvc._
 
@@ -10,19 +10,29 @@ object Search extends Controller with Secured {
 
   def index(page: Int, count: Int, query: String, sort: Option[String] = None, order: Option[String] = None) = IsAuthenticated() { implicit request =>
 
-    val filters = request.queryString filterKeys { key =>
-      SearchModel.ticketFilterMap.get(key).isDefined
+    val existingTicket: Option[EditTicket] = if(TicketModel.isValidTicketId(query)) {
+      TicketModel.getById(query)
+    } else {
+      None
     }
 
-    val userId = request.user.id.get
-    val sort = request.queryString.get("sort").map({ vals => Some(vals.head) }).getOrElse(None);
-    val order = request.queryString.get("order").map({ vals => Some(vals.head) }).getOrElse(None);
-    val q = emp.util.Search.SearchQuery(
-      userId = userId, page = page, count = count, query = query,
-      filters = filters, sortBy = sort, sortOrder = order
-    )
-    val result = SearchModel.searchTicket(q)
+    existingTicket.map({ t =>
+      Redirect(routes.Ticket.item("comments", t.ticketId.get))
+    }).getOrElse({
+      val filters = request.queryString filterKeys { key =>
+        SearchModel.ticketFilterMap.get(key).isDefined
+      }
 
-    Ok(views.html.search.index(filters, result)(request))
+      val userId = request.user.id.get
+      val sort = request.queryString.get("sort").map({ vals => Some(vals.head) }).getOrElse(None);
+      val order = request.queryString.get("order").map({ vals => Some(vals.head) }).getOrElse(None);
+      val q = emp.util.Search.SearchQuery(
+        userId = userId, page = page, count = count, query = query,
+        filters = filters, sortBy = sort, sortOrder = order
+      )
+      val result = SearchModel.searchTicket(q)
+
+      Ok(views.html.search.index(filters, result)(request))
+    })
   }
 }
